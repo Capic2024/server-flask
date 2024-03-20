d5c3362f5e3961b2207c4f5786aac30b5da271e1
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-03-20 20:09:12 +0900

    feat : ip 추가

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port='5000')
 
 @app.route('/sendImg',methods=['GET'])
 def sendImg():
