12169bb06f38e4a74b0f7dbc6cad0656e003fd89
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-03-20 23:31:12 +0900

    feat : s3 연결 코드 구현

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,jsonify
+from connection import s3_connection
+from config import BUCKET_NAME
 
 app = Flask(__name__)
 
@@ -7,10 +9,21 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/image', methods=['POST'])
+def test_image():
+    s3 = s3_connection()
+    try:
+        with open('dd.jpeg', 'rb') as image_file:
+            s3.put_object(
+                Bucket=BUCKET_NAME,
+                Body=image_file,
+                Key='dd.jpeg',
+                ContentType='image/jpeg'
+            )
+        return jsonify({'success': True})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
 
-@app.route('/sendImg',methods=['GET'])
-def sendImg():
-    return jsonify({'class_id':'IMGAGE'})
