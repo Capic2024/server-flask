a0d84aa81de14d65979dd97ebe658573bec81cc7
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-03-21 16:20:10 +0900

    test : cicd test

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def hello_world():  # put application's code here
 def test():  # put application's code here
     return 'post test'
 
+@app.route('/test2', methods=['GET'])
+def test2():  # put application's code here
+    return 'post test2'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
