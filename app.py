6b41d8543854507ce0dec0438a8c31c24e41dd7d
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-03-21 19:02:13 +0900

    test : 주석
    갑자기 server 안되서 주석처리

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import (Flask, jsonify)
-from connection import s3_connection
-import os
+# from connection import s3_connection
+# import os
 
 app = Flask(__name__)
 
@@ -22,20 +22,20 @@ def test2():  # put application's code here
 def test3():  # put application's code here
     return 'post test3'
 
-@app.route('/image', methods=['POST'])
-def test_image():
-    s3 = s3_connection()
-    try:
-        with open('dd.jpeg', 'rb') as image_file:
-            s3.put_object(
-                Bucket=os.getenv('BUCKET_NAME'),
-                Body=image_file,
-                Key='dd.jpeg',
-                ContentType='image/jpeg'
-            )
-        return jsonify({'success': True})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+# @app.route('/image', methods=['POST'])
+# def test_image():
+#     s3 = s3_connection()
+#     try:
+#         with open('dd.jpeg', 'rb') as image_file:
+#             s3.put_object(
+#                 Bucket=os.getenv('BUCKET_NAME'),
+#                 Body=image_file,
+#                 Key='dd.jpeg',
+#                 ContentType='image/jpeg'
+#             )
+#         return jsonify({'success': True})
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
