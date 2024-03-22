193413025453697648268370b108eb68caba972b
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-03-22 14:06:31 +0900

    fix : 실행코드 수정, 주석 제거

--- connection.py
+++ connection.py
@@ -1,9 +1,9 @@
-# import boto3
-# import os
-#
-#
-# def s3_connection():
-#     s3 = boto3.client('s3',
-#                       aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-#                       aws_secret_access_key=os.getenv('AWS_SECRET_KEY'))
-#     return s3
+import boto3
+import os
+
+
+def s3_connection():
+    s3 = boto3.client('s3',
+                      aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+                      aws_secret_access_key=os.getenv('AWS_SECRET_KEY'))
+    return s3
