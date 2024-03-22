646494d27c3d30cfedc44b91b885fbc743b2cea8
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-03-22 15:30:47 +0900

    docs : s3 code 주석 제거

--- connection.py
+++ connection.py
@@ -1,9 +1,9 @@
-# import boto3
-# import os
-#
-#
-# def s3_connection():
-#     s3 = boto3.client('s3',
-#                       aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
-#                       aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'))
-#     return s3
+import boto3
+import os
+
+
+def s3_connection():
+    s3 = boto3.client('s3',
+                      aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+                      aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'))
+    return s3
