a2b1e474e84315109f388a3ca451b4db5412c8c2
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-03-22 16:46:11 +0900

    fet : connection s3 error code

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 load_dotenv()
 
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
@@ -17,6 +18,7 @@ def hello_world():  # put application's code here
 @app.route('/test', methods=['POST'])
 def test():  # put application's code here
     return 'post test'
+
 
 @app.route('/test2', methods=['GET'])
 def test2():  # put application's code here
@@ -39,23 +41,27 @@ def test_image():
         client.upload_file(file_name, bucket, key)  # 파일 저장
         return jsonify({'success': True})
 
+    except TypeError as e:
+        logging.error("TypeError 발생: %s", str(e))
+        return jsonify({'error': "TypeError 발생: " + str(e)})
     except Exception as e:
-        logging.error("Error uploading image: %s", str(e))
-        return jsonify({'error': str(e)})
+        logging.error("일반 오류 발생: %s", str(e))
+        return jsonify({'error': "일반 오류 발생: " + str(e)})
 
-    # s3 = s3_connection()
-    # try:
-    #     with open('dd.jpeg', 'rb') as image_file:
-    #         s3.put_object(
-    #             Bucket=os.environ.get('BUCKET_NAME'),
-    #             Body=image_file,
-    #             Key='dd.jpeg',
-    #             ContentType='image/jpeg'
-    #         )
-    #     return jsonify({'success': True})
-    # except Exception as e:
-    #     logging.error("Error uploading image: %s",str(e))
-    #     return jsonify({'error': str(e)})
+
+# s3 = s3_connection()
+# try:
+#     with open('dd.jpeg', 'rb') as image_file:
+#         s3.put_object(
+#             Bucket=os.environ.get('BUCKET_NAME'),
+#             Body=image_file,
+#             Key='dd.jpeg',
+#             ContentType='image/jpeg'
+#         )
+#     return jsonify({'success': True})
+# except Exception as e:
+#     logging.error("Error uploading image: %s",str(e))
+#     return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
