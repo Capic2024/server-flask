476df7e052ccadd4d1d8ebe49689e9568f7681f3
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-03-26 17:22:31 +0900

    #8 feat : video api 구현
    springboot와 파일 주고 받는 코드 구현

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import (Flask, jsonify, send_file)
-from dotenv import load_dotenv
-import boto3
-# from connection import s3_connection
+from flask import (Flask, request, send_file)
+import werkzeug
 import os
-import logging
 
 app = Flask(__name__)
-load_dotenv()
-logging.basicConfig(level=logging.ERROR)
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -23,6 +18,22 @@ def test():  # put application's code here
 def test2():  # put application's code here
     return '03.25 22:40'
 
+
+@app.route('/video', methods=['POST'])
+def video():  # put application's code here
+    if 'file' not in request.files:
+        return "No file part", 400
+    file = request.files['file']
+    filename = werkzeug.utils.secure_filename(file.filename)
+
+    # 파일 저장 경로 설정
+    save_path = os.path.join("your/storage/path/", filename)
+    file.save(save_path)
+
+    # 파일 처리 로직을 여기에 작성 (예시에서는 단순 저장만 하고 있음)
+
+    # 처리한 파일을 Spring Boot로 반환
+    return send_file(save_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
